7d53493dbd9995416da43f15567bec4d0fd812a2
Trilomancer <jjwoodcook@gmail.com>
2025-03-23 19:31:23 -0400

    screw you :(

--- scripts/Singleton.gd
+++ scripts/Singleton.gd
@@ -1,6 +1,8 @@
 extends Node
 
 
+
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("f11"):
 		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,11 +1,49 @@
 extends CanvasLayer
 
+@onready var settings_menu: Control = $SettingsMenu
+@onready var paused_menu: Control = $PausedMenu
+@onready var quit_label: Label = $QuitLabel
+@onready var darken: TextureRect = $Darken
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	process_mode = PROCESS_MODE_ALWAYS
+	settings_menu.visible = false
+	paused_menu.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("pause"):
+		if paused_menu.visible == false and settings_menu.visible == false:
+			get_tree().paused = true
+			paused_menu.visible = true
+		elif paused_menu.visible == true:
+			get_tree().paused = false
+			paused_menu.visible = false
+		elif settings_menu.visible == true:
+			paused_menu.visible = true
+			settings_menu.visible = false
+		
+
+
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	paused_menu.visible = false
+
+
+func _on_quit_pressed() -> void:
+	paused_menu.visible = false
+	settings_menu.visible = false
+	darken.modulate = Color.BLACK
+	quit_label.visible = true
+	get_tree().quit
+
+
+func _on_settings_pressed() -> void:
+	paused_menu.visible = false
+	settings_menu.visible = true
+
+
+func _on_back_pressed() -> void:
+	paused_menu.visible = true
+	settings_menu.visible = false
