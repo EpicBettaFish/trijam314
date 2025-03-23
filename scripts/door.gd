3ede55e75e9b84b400cd97426ba81f9d35841297
EpicBettaFish <epicbetafish@gmail.com>
2025-03-23 19:27:32 -0400

    changes of some variety

--- scripts/door.gd
+++ scripts/door.gd
@@ -4,12 +4,14 @@ var tween
 @onready var doorCollider = $Collider
 
 func activate() -> void:
+	$Collider/Sprite2D.frame = 1
 	if tween:
 		tween.kill()
 	tween = create_tween().set_trans(Tween.TRANS_LINEAR)
-	tween.tween_property(doorCollider, "position", Vector2(0, -21), 0.5)
+	tween.tween_property(doorCollider, "position", Vector2(0, -18), 0.5)
 
 func deactivate() -> void:
+	$Collider/Sprite2D.frame = 0
 	if tween:
 		tween.kill()
 	tween = create_tween().set_trans(Tween.TRANS_LINEAR)
