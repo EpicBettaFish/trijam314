8804b3d92070d3e82338daec5c57119a78e29cfe
Trilomancer <jjwoodcook@gmail.com>
2025-03-23 16:57:31 -0400

    grasshopper (cocaine)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,45 @@
 extends CharacterBody2D
 
+@onready var player_animator: AnimationPlayer = $PlayerAnimator
+@onready var player_sprite: Sprite2D = $PlayerSprite
 
-const SPEED = 300.0
-var jump_height = -400.0
+const SPEED = 50.0
+var jump_height = -200.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_height
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# player controls
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction == -1:
+			player_sprite.flip_h = 1
+		elif direction == 1:
+			player_sprite.flip_h = 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# jump
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_height
+		
+	# player animations
+	if is_on_floor():
+		if direction:
+			player_animator.play("walk")
+		else:
+			player_animator.play("idle")
+	else:
+		if velocity.y > 0:
+			player_animator.play("jump descend")
+		elif velocity.y < 0:
+			player_animator.play("jump ascend")
+
+		var real_grav = get_gravity()
+		if abs(velocity.y) < 30:
+			real_grav /= 6
+		velocity += real_grav * delta
+
+
 
 	move_and_slide()
