23bb7e0e9dc8074fc672fe850febbcbe3fc93f72
EpicBettaFish <epicbetafish@gmail.com>
2025-03-23 16:42:36 -0400

    god

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
 var tween
+@onready var doorCollider = $Collider
 
 func activate() -> void:
 	if tween:
 		tween.kill()
+	tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+	tween.tween_property(doorCollider, "position", Vector2(0, -120), 1)
+
+func deactivate() -> void:
+	if tween:
+		tween.kill()
+	tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+	tween.tween_property(doorCollider, "position", Vector2(0, 0), 1)
