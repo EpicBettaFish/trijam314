7ec2e710393d7cd2fe07bc9a58076ee2264055c7
EpicBettaFish <epicbetafish@gmail.com>
2025-03-23 17:21:45 -0400

    turret

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+var myTurret
+const SPEED = 5.0
 
 func _physics_process(delta: float) -> void:
-	global_position += Vector2(1,0).rotated(rotation)
+	global_position += Vector2(SPEED,0).rotated(rotation)
+
+func _on_body_entered(body: Node2D) -> void:
+	if myTurret != body:
+		queue_free()
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,7 +1,21 @@
 extends StaticBody2D
 
 @onready var bulletDir = $BULLETDIR
-@export var shootingInterval = 0.2
+@export var shootingInterval = 0.35
+
+var bulletScene = preload("res://scenes/bullet.tscn")
+
+func _ready() -> void:
+	timerLoop()
+
+func timerLoop() -> void:
+	await get_tree().create_timer(shootingInterval).timeout
+	shoot()
+	timerLoop()
 
 func shoot() -> void:
-	pass
+	var newBullet = bulletScene.instantiate()
+	newBullet.rotation = bulletDir.rotation
+	newBullet.myTurret = self
+	newBullet.global_position = global_position
+	get_parent().add_child(newBullet)
