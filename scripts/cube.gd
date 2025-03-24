404185b7d4c2aebac01cf25b9e0593bca890abcb
EpicBettaFish <epicbetafish@gmail.com>
2025-03-23 22:54:49 -0400

    balls moment

--- scripts/cube.gd
+++ scripts/cube.gd
@@ -1,10 +1,5 @@
 extends RigidBody2D
 
-@onready var unstuck = $Unstuck
-
-func  _physics_process(delta: float) -> void:
-	unstuck.global_position = global_position
-
 func _on_left_body_entered(body: Node2D) -> void:
 	if body != self:
 		angular_velocity += 35
