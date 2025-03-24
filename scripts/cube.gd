82f7e98209493123ccd1c454c4c5abb5a608d624
EpicBettaFish <epicbetafish@gmail.com>
2025-03-23 21:36:55 -0400

    bullet new sprite

--- scripts/cube.gd
+++ scripts/cube.gd
@@ -8,9 +8,9 @@ func  _physics_process(delta: float) -> void:
 func _on_left_body_entered(body: Node2D) -> void:
 	if body != self:
 		angular_velocity += 35
-		linear_velocity.x += 40
+		linear_velocity.x += 100
 
 func _on_right_body_entered(body: Node2D) -> void:
 	if body != self:
 		angular_velocity -= 35
-		linear_velocity.x += 40
+		linear_velocity.x += 100
