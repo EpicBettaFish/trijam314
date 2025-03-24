36f677f6ad11b8cbc5f1054f8f2a129e960264ac
EpicBettaFish <epicbetafish@gmail.com>
2025-03-23 21:01:42 -0400

    sprite changes

--- scripts/lever.gd
+++ scripts/lever.gd
@@ -4,5 +4,6 @@ extends StaticBody2D
 
 func _on_lever_handle_body_entered(body: Node2D) -> void:
 	if body.is_in_group("canPushButton"):
+		$LeverBase.frame = 1
 		connectedNode.activate()
 		$LeverHandle/CollisionShape2D.queue_free()
