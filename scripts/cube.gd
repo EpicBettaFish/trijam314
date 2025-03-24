afec5a5e6e353da10fc5c786b1414e013fb600ca
EpicBettaFish <epicbetafish@gmail.com>
2025-03-23 22:36:31 -0400

    cube change

--- scripts/cube.gd
+++ scripts/cube.gd
@@ -9,8 +9,10 @@ func _on_left_body_entered(body: Node2D) -> void:
 	if body != self:
 		angular_velocity += 35
 		linear_velocity.x += 100
+		linear_velocity.y -= 40
 
 func _on_right_body_entered(body: Node2D) -> void:
 	if body != self:
 		angular_velocity -= 35
 		linear_velocity.x += 100
+		linear_velocity.y -= 40
